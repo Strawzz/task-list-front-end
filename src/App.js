ce205f5be6970a9a644019e0afc6f0cd60d40e50
Say Ryder <lindz.ryder@gmail.com>
2023-06-15 16:08:09 -0400

    implements delete task

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
 
@@ -16,13 +16,20 @@ const TASKS = [
 ];
 
 const App = () => {
+  const [tasks, setTasks] = useState(TASKS);
+
+  const deleteTask = id => {
+   console.log('Delete task with id:', id);
+   setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>{<TaskList tasks={TASKS} deleteTask={deleteTask}/>}</div>
       </main>
     </div>
   );
--- src/components/Task.js
+++ src/components/Task.js
@@ -3,19 +3,25 @@ import PropTypes from 'prop-types';
 
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
+const Task = ({ id, title, isComplete, deleteTask }) => {
   const [complete, setComplete] = useState(isComplete);
+  const [removed, setRemoved] = useState(false);
   const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
 
+  const handleDeleteTask = () => {
+    deleteTask(id);
+    setRemoved(true);
+  };
+
   return (
-    <li className="tasks__item">
+    <li className={`tasks__item ${removed ? 'removed' : ''}`}>
       <button
         className={`tasks__item__toggle ${buttonClass}`}
         onClick={() => setComplete(!complete)}
       >
         {title}
       </button>
-      <button className="tasks__item__remove button">x</button>
+      <button className="tasks__item__remove button" onClick={handleDeleteTask}>x</button>
     </li>
   );
 };
@@ -24,6 +30,7 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
+  deleteTask: PropTypes.func.isRequired,
 };
 
 export default Task;
